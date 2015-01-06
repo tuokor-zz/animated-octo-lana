431d7420f139a9c90066ca1a33a99a32d7455a2a
Tuomas Korjonen <tuokor@gmail.com>
2015-01-06 13:30:38 +0200

    refactored datasender as a class

--- datasender.py
+++ datasender.py
@@ -3,47 +3,65 @@ from multiprocessing.managers import SyncManager
 import time
 import signal
 
-def mgr_init():
-    signal.signal(signal.SIGINT, signal.SIG_IGN)
-    print("initialized manager")
+class DataSender:
 
-running = True
-def up(que):
-    def stop(val,val2):
-        global running
-        print("stop called {0}".format(running))
-        running = False
-        print("running={0}".format(running))
 
-    signal.signal(signal.SIGINT, stop)
-    print('datauploader started')
-    while running or not que.empty():
-        print("state:{0} que={1}".format(running, que.empty()))
-        item = que.get(True)
-        print("got item={0}".format(item))
-        que.task_done()
-        time.sleep(2)
-    print("datauploader process terminating...")
+    def __init__(self):
+        self.phant = dict()
+        self.running = True
+        self._manager = SyncManager()
+        self._manager.start(self._mgr_init())
+        self._que = self._manager.Queue()
+        self._process = Process(target=self.up, args=(self._que,))
+        self._process.start()
 
+    def _mgr_init(self):
+        signal.signal(signal.SIGINT, signal.SIG_IGN)
+        print("initialized manager")
+
+    def up(self,que):
+        def stop(val,val2):
+            print("stop called {0}".format(self.running))
+            self.running = False
+            print("running={0}".format(self.running))
+
+        signal.signal(signal.SIGINT, stop)
+        print('datauploader started')
+        while self.running or not que.empty():
+            print("state:{0} que={1}".format(self.running, que.empty()))
+            item = que.get(True)
+            print("got item={0}".format(item))
+            que.task_done()
+            time.sleep(2)
+        print("datauploader process terminating...")
+
+    def send(self, data):
+        self._que.put(data)
+
+    def httpsend(self, data):
+        postdata = urllib.urlencode(data)
+        headers = {'Phant-Private-Key': phant['privateKey'] }
+        req = urllib2.Request(phant['inputUrl'], postdata, headers)
+        res = urllib2.urlopen(req)
+        content = resp.read()
+        print("response: {0}".format(content))
+    
+    def stop(self):
+        print("shutting down sender")
+        self._que.join()
+        self._process.terminate()
 
 if __name__ == '__main__':
-    manager = SyncManager()
-    manager.start(mgr_init)
-    que = manager.Queue()
-    p = Process(target=up, args=(que,))
-    p.start()
+    sender = DataSender()
     val = 0
     try:
         while True:
             val = val+1
             print("adding new item to que={0}".format(val))
-            que.put("item {0}".format(val))
+            sender.send("item {0}".format(val))
             if val % 2 == 0:
                 print("main thread will sleep now for 3 seconds")
                 time.sleep(3)
     except KeyboardInterrupt:
-        print("shutting down waiting que")
-        que.join()
-        print("exiting process")
-        p.terminate()
-
+        print("keyboard interrupt")
+        sender.stop()
